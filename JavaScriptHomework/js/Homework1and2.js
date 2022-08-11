e514d32db93556919fc51e3f9604298bc81f7536
紹瑋 藍 <maimaisatt@gmail.com>
2022-08-11 19:44:22 +0800

    20220811_homework4start

--- JavaScriptHomework/js/Homework1and2.js
+++ JavaScriptHomework/js/Homework1and2.js
@@ -7,28 +7,28 @@ let incorrectIcon = "<img src='../img/incorrect.png'/>";
 function checkName() {
     let name = document.getElementById("name").value;
     let spanName = document.getElementById("spanName");
-    let re = /^[\u4e00-\u9fa5]{2,}$/;
-    if (re.test(name)) {
-        spanName.innerHTML = `${correctIcon}Correct`;
-    } else {
-        spanName.innerHTML = `${incorrectIcon}Incorrect`;
-    }
-    
-    //let isChi = true;
-    //if (name == "") {
-    //    spanName.innerHTML = `${incorrectIcon}姓名不能空白`;
-    //} else if (name.length < 2) {
-    //    spanName.innerHTML = `${incorrectIcon}至少要兩個中文字`;
+    //let re = /^[\u4e00-\u9fa5]{2,}$/;
+    //if (re.test(name)) {
+    //    spanName.innerHTML = `${correctIcon}Correct`;
     //} else {
-    //    for (let i = 0; i < name.length; i++) {
-    //        if (name.charCodeAt(i) < 0x4E00 || name.charCodeAt(i) > 0x9FA5) {
-    //            spanName.innerHTML = `${incorrectIcon}必須全部為中文字`;
-    //            isChi = false;
-    //            break;
-    //        }
-    //    }
-    //    if (isChi) spanName.innerHTML = `${correctIcon}Correct`;
+    //    spanName.innerHTML = `${incorrectIcon}Incorrect`;
     //}
+    
+    let isChi = true;
+    if (name == "") {
+        spanName.innerHTML = `${incorrectIcon}姓名不能空白`;
+    } else if (name.length < 2) {
+        spanName.innerHTML = `${incorrectIcon}至少要兩個中文字`;
+    } else {
+        for (let i = 0; i < name.length; i++) {
+            if (name.charCodeAt(i) < 0x4E00 || name.charCodeAt(i) > 0x9FA5) {
+                spanName.innerHTML = `${incorrectIcon}必須全部為中文字`;
+                isChi = false;
+                break;
+            }
+        }
+        if (isChi) spanName.innerHTML = `${correctIcon}Correct`;
+    }
 }
 function checkPwd() {
     let pwd = document.getElementById("pwd").value.toUpperCase();
@@ -47,26 +47,7 @@ function checkPwd() {
             spanPwd.innerHTML = `${incorrectIcon}密碼格式錯誤`;
         }
     }
-    //let re = /[!@#$%^&*]{1,}/;
-    //let flag1 = false, flag2 = false, flag3 = false;
-    //if (pwd == "") {
-    //    spanPwd.innerHTML = `${incorrectIcon}密碼不能空白`;
-    //} else if (pwd.length < 6) {
-    //    spanPwd.innerHTML = `${incorrectIcon}密碼需要大於6個字`;
-    //} else {
-    //    for (let i = 0; i < pwd.length; i++) {
-    //        if (pwd.charAt(i) >= "A" && pwd.charAt(i) <= "Z") {
-    //            flag1 = true;
-    //        } else if (pwd.charAt(i) >= "0" && pwd.charAt(i) <= "9") {
-    //            flag2 = true;
-    //        } else if (re.test(pwd.charAt(i))) {
-    //            flag3 = true;
-    //        }
-    //        if (flag1 && flag2 && flag3) break;
-    //    }
-    //    if (flag1 && flag2 && flag3) spanPwd.innerHTML = `${correctIcon}Correct`;
-    //    else spanPwd.innerHTML = `${incorrectIcon}密碼格式錯誤`;
-    //}
+    
 }
 function checkDate() {
     let myDate = document.getElementById("date").value;
---
+++ JavaScriptHomework/js/Homework4.js
@@ -0,0 +1,11 @@
+﻿window.setInterval(PlayImage, 5000);
+document.getElementById("play").
+
+let imageNum = 2;
+function PlayImage() {
+    if (imageNum > 10) {
+        imageNum = 1;
+    }
+    document.getElementById("article4img").src = `img/homework4/${imageNum}.jpg`;
+    imageNum += 1;
+}
