e87b0cfa478b2611e525fab595e1fda45f49f051
紹瑋 藍 <maimaisatt@gmail.com>
2022-08-14 01:56:44 +0800

    20220814_homework5finish

--- JavaScriptHomework/js/Homework5.js
+++ JavaScriptHomework/js/Homework5.js
@@ -1,6 +1,8 @@
 ﻿document.addEventListener("DOMContentLoaded", init);
 document.getElementById("selectYear").addEventListener("change", ChangeYearOrMonth);
 document.getElementById("selectMonth").addEventListener("change", ChangeYearOrMonth);
+document.getElementById("selectDate").addEventListener("change", ChangeDate);
+
 
 let strYear = "";
 for (let i = 2010; i <= 2025; i++) {
@@ -13,6 +15,8 @@ for (let i = 1; i <= 12; i++) {
     strMonth += `<option value="${i}" id="m${i}">${i}</option>`
 }
 document.getElementById("selectMonth").innerHTML = strMonth;
+let IsSelectDate = false;
+
 let strDate = "";
 let today;
 let year;
@@ -28,20 +32,71 @@ function init() {
     document.getElementById("selectYear").value = year;
     document.getElementById("selectMonth").value = month;
     AddDate();
-    document.getElementById("selectDate").value = date;
+    document.getElementById("selectDate").value = "1";
+    ShowCalender();
 }
 
 function ChangeYearOrMonth() {
     year = document.getElementById("selectYear").value;
     month = document.getElementById("selectMonth").value;
-   /* date = document.getElementById("")*/
     AddDate();
-    document.getElementById("informationContent").innerHTML = `您選擇的日期是 ${year} 年 ${month} 月`;
+    date = document.getElementById("selectDate").value;
+    document.getElementById("informationContent").innerHTML = `您選擇的日期是 ${year} 年 ${month} 月 ${date} 日`;
+    ShowCalender();
 }
 
+function ChangeDate() {
+    if (IsSelectDate) ShowCalender();
+    year = document.getElementById("selectYear").value;
+    month = document.getElementById("selectMonth").value;
+    date = document.getElementById("selectDate").value;
+    document.getElementById("informationContent").innerHTML = `您選擇的日期是 ${year} 年 ${month} 月 ${date} 日`;
+    document.getElementById(`c${date}`).style.backgroundColor = "#B9B973";
+    document.getElementById(`c${date}`).style.color = "white";
+    IsSelectDate = true;
+    
+}
 
 function AddDate() {
     strDate = "";
+    let count = DecideDayCount();
+    
+    for (let i = 1; i <= count; i++) {
+        strDate += `<option value="${i}" id="d${i}">${i}</option>`;
+    }
+    document.getElementById("selectDate").innerHTML = strDate;
+}
+
+function ShowCalender() {
+    let count = DecideDayCount();
+    let firstDay = new Date(`${year}/${month}/1`).getDay();
+    let weekcount = (Math.ceil((count - (7 - firstDay)) / 7)) + 1;
+    let start = 1;
+    let str = "";
+    for (let i = 1; i <= weekcount; i++) {
+        str += "<tr>";
+        for (let j = 0; j < 7; j++) {
+            if (i == 1 && j >= firstDay) {
+                str += `<td id="c${start}">${start}</td>`;
+                start += 1;
+            } else if (i > 1 && start <= count) {
+                str += `<td id="c${start}">${start}</td>`;
+                start += 1;
+            } else {
+                str += `<td> </td>`;
+            }
+        }
+        str += "</tr>";
+    }
+    document.getElementById("tbody").innerHTML = str;
+    for (let i = 1; i <= count; i++) {
+        document.getElementById(`c${i}`).addEventListener("mouseover", MouseOverCalender);
+        document.getElementById(`c${i}`).addEventListener("mouseout", MouseOutCalender);
+        document.getElementById(`c${i}`).addEventListener("click", ClickCalender);
+    }
+}
+
+function DecideDayCount() {
     let count;
     if (bigMonth.indexOf(parseInt(month)) != -1) {
         count = 31;
@@ -52,13 +107,24 @@ function AddDate() {
         if (day1.getDate() != 29) count = 28;
         else count = 29;
     }
-    for (let i = 1; i <= count; i++) {
-        strDate += `<option value="${i}" id="d${i}">${i}</option>`;
-    }
-    document.getElementById("selectDate").innerHTML = strDate;
+    return count;
 }
 
-
+function MouseOverCalender() {
+    if (date == this.id.substr(1)) return;
+    document.getElementById(this.id).style.backgroundColor = "#DEDEBE";
+    document.getElementById(this.id).style.color = "white";
+    document.getElementById(this.id).style.cursor = "pointer";
+}
+function MouseOutCalender() {
+    if (date == this.id.substr(1)) return;
+    document.getElementById(this.id).style.backgroundColor = "#F0F0F0";
+    document.getElementById(this.id).style.color = "black";
+}
+function ClickCalender() {
+    document.getElementById("selectDate").value = this.id.substr(1);
+    ChangeDate()
+}
 
 
 
